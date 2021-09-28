a36ab595b3307fd55dfed56bbf04319edcbbda08
mattegan111 <mnegan93@gmail.com>
2021-09-29 10:55:34 +1300

    Adds 'delete contact' to frontend

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -48,7 +48,9 @@ const ContactState = props => {
   }
 
   // Delete Contact
-
+  const deleteContact = id => {
+    dispatch({ type: DELETE_CONTACT, payload: id })
+  }
   // Set Current Contact
 
   // Clear Current Contact
@@ -63,7 +65,8 @@ const ContactState = props => {
     <contactContext.Provider
     value={{
       contacts: state.contacts,
-      addContact
+      addContact,
+      deleteContact
     }}>
       { props.children }
     </contactContext.Provider>

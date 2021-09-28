f081881b537dbfb4053eada138bc60afda1c10ca
mattegan111 <mnegan93@gmail.com>
2021-09-29 10:37:37 +1300

    Adds 'add contact' to frontend

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from 'react'
-import uuid from 'uuid'
+import {v4 as uuid} from 'uuid'
 import contactContext from './contactContext'
 import contactReducer from './contactReducer'
 import {
@@ -42,6 +42,10 @@ const ContactState = props => {
   const [state, dispatch] = useReducer(contactReducer, initialState)
 
   // Add Contact
+  const addContact = contact => {
+    contact.id = uuid()
+    dispatch({ type: ADD_CONTACT, payload: contact })
+  }
 
   // Delete Contact
 
@@ -58,7 +62,8 @@ const ContactState = props => {
   return (
     <contactContext.Provider
     value={{
-      contacts: state.contacts
+      contacts: state.contacts,
+      addContact
     }}>
       { props.children }
     </contactContext.Provider>

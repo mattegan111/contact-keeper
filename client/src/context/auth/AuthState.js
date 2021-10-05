7602ba89018af51c1105a77158c5af2291420966
Matthew Egan <mnegan93@gmail.com>
2021-10-05 13:32:28 +1300

    Adds 'register' functionality

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -1,4 +1,5 @@
 import React, { useReducer } from 'react'
+import axios from 'axios'
 import authContext from './authContext'
 import authReducer from './authReducer'
 import {
@@ -18,20 +19,46 @@ const AuthState = props => {
     isAuthenticated: null,
     loading: true,
     user: null,
-    error: null
+    error: null,
   }
 
   const [state, dispatch] = useReducer(authReducer, initialState)
 
   // Load User
+  const loadUser = () => console.log('load user')
 
   // Register User
+  const register = async (formData) => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    try {
+      const res = await axios.post('/api/users', formData, config)
+      dispatch({
+        type: REGISTER_SUCCESS,
+        payload: res.data
+      })
+    } catch (err) {
+      dispatch({
+        type: REGISTER_FAIL,
+        payload: err.response.data.msg
+      })
+    }
+  }
 
   // Login User
+  const login = () => console.log('login')
 
   // Logout
+  const logout = () => console.log('logout')
+
 
   // Clear Errors
+  const clearErrors = () => console.log('clear errors')
+
 
   return (
     <authContext.Provider
@@ -41,7 +68,11 @@ const AuthState = props => {
       loading: state.loading,
       user: state.user,
       error: state.error,
-
+      register,
+      loadUser,
+      login,
+      logout,
+      clearErrors
     }}>
       { props.children }
     </authContext.Provider>
